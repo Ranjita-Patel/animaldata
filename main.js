4d76cbc128de706eb8ad0a83e5b5aea10d8e2808
Ranjita-Patel <ranjitap59@gmail.com>
2017-02-20 20:08:46 +0530

    firstupload

---
+++ main.js
@@ -0,0 +1,17 @@
+var thePets = [
+	{
+		"name": "meowsalot",
+		"species": "cat",
+		"favfood": "tuna"
+	}, 
+	{
+		"name": "barky",
+		"species": "dog",
+		"favfood": "chicken"
+	}, 
+	{
+		"name": "horsy",
+		"species": "Horse",
+		"favfood": "jaggery"
+	}
+]
